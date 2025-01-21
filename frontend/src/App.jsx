10b067bae5444233671f46db6e88a134c2949f64
praveen-saharan <praveen.saharan@accenture.com>
2025-01-21 11:53:13 +0900

    test purpose

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline text-red-300 uppercase">
-      Hello world!
+      Railway Ticketing System
     </h1>
   )
 }
