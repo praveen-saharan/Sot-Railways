f3cb3a9a0d1194256df2f74b03126e61828717e2
praveen-saharan <praveen.saharan@accenture.com>
2025-01-23 12:03:13 +0900

    Correct Error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
-
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import Login from "./Pages/Login";
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './Pages/Home';
-import Contact from './Pages/Contact';
-import Login from './Pages/Login';
-
 
 import NotExist from "./Components/NotExist";
 import Navbar from "./Components/Navbar";
@@ -30,9 +22,8 @@ import TicketEdit from "./Pages/Admin/TicketEdit";
 function App() {
   return (
     <Router>
-
       <div className="min-h-screen flex flex-col">
-      <Navbar />
+        <Navbar />
 
         <div className="flex-1 text-center mt-22">
           <Routes>
@@ -41,51 +32,24 @@ function App() {
             <Route path="/contact" element={<Contact />} />
 
             {/* User pages */}
-            {/* <Switch> */}
             <Route path="/train-schedule" element={<TrainSchedule />} />
             <Route path="/booking" element={<Booking />} />
             <Route path="/booking-summary" element={<BookingSummary />} />
             <Route path="/payment" element={<Payment />} />
             <Route path="/ticket" element={<Ticket />} />
-            {/* </Switch> */}
 
             {/* Admin pages */}
             <Route path="/admin/passenger-list" element={<PassengerList />} />
-            <Route path="/admin/financial-reports" element={<FinancialReports />}/>
+            <Route path="/admin/financial-reports" element={<FinancialReports />} />
             <Route path="/admin/schedule-edit" element={<ScheduleEdit />} />
             <Route path="/admin/ticket-edit" element={<TicketEdit />} />
 
+            {/* Catch-all route for non-existent pages */}
             <Route path="*" element={<NotExist />} />
           </Routes>
-
         </div>
-      <Footer />
 
-      <Navbar/>
-      <div className="text-center mt-22">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/contact" element={<Contact />} />
-
-          {/* User pages */}
-          <Route path="/train-schedule" element={<TrainSchedule />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/booking-summary" element={<BookingSummary />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/ticket" element={<Ticket />} />
-
-          {/* Admin pages */}
-          <Route path="/admin/passenger-list" element={<PassengerList />} />
-          <Route path="/admin/financial-reports" element={<FinancialReports />} />
-          <Route path="/admin/schedule-edit" element={<ScheduleEdit />} />
-          <Route path="/admin/ticket-edit" element={<TicketEdit />} />
-
-
-
-          <Route path="*" element={<NotExist />} />
-        </Routes>
-
+        <Footer />
       </div>
     </Router>
   );
